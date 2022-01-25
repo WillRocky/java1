affb73c31d49f0d0688e6c80f0ae0d38178c7228
WillRocky <willrocky.2018@gmail.com>
2022-01-25 18:38:13 +0300

    learn option

--- src/class1.java
+++ src/class1.java
@@ -3,5 +3,6 @@ public class class1 {
         System.out.println("Hello World");
         System.out.println("WillRocky");
         System.out.println("12345");
+        System.out.println("original");
     }
 }
